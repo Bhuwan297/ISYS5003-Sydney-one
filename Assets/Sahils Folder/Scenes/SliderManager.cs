4528d8f790091146817003added84f8c84df0a1b
Sahil Gaire Sharma <sahilgaire.2@gmail.com>
2024-04-14 16:44:12 +1000

    Adjustments

--- Assets/Sahils Folder/Scenes/SliderManager.cs
+++ Assets/Sahils Folder/Scenes/SliderManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SpawnInteractable_Adam : MonoBehaviour
+public class SliderManager : MonoBehaviour
 {
     private Slider scaleSlider;
     private Slider rotateSlider;
