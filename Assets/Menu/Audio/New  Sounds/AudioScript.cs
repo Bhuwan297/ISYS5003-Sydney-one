2339912c58f3318e0f464ce9b6b281732b525278
Sahil Gaire Sharma <sahilgaire.2@gmail.com>
2024-04-15 04:20:02 +1000

    Added more new sounds

---
+++ Assets/Menu/Audio/New  Sounds/AudioScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    public AudioSource Drop2;
+    public AudioSource Drop3;
+    public AudioSource Drop4;
+    public AudioSource Drop5;
+
+    public void PlayDrop2() {
+        Drop2.Play ();
+
+    }
+
+     public void PlayDrop3() {
+        Drop3.Play ();
+
+    }
+    
+    public void PlayDrop4() {
+        Drop4.Play ();
+
+    }
+    
+    public void PlayDrop5() {
+        Drop5.Play ();
+
+    }
+}
